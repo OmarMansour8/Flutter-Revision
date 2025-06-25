3b34c1b98240b03e18263ee26ed42ccf090acfbb
Omar Mansour <102729790+OmarMansour8@users.noreply.github.com>
2025-06-25 11:46:13 +0300

    Launching Onboarding Screen

--- recap_project/lib/features/onboarding/onboarding_screen.dart
+++ recap_project/lib/features/onboarding/onboarding_screen.dart
@@ -1,27 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:recap_project/features/onboarding/widgets/doc_logo_and_name_widget.dart';
 
 class Onboardingscreen extends StatelessWidget {
   const Onboardingscreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Onboarding Screen')),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text('Welcome to the Onboarding Screen!'),
-              ElevatedButton(
-                onPressed: () {
-                  // Navigate to the next screen or perform an action
-                },
-                child: const Text('Get Started'),
-              ),
-            ],
-          ),
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.only(top: 30.h),
+          child: Column(children: [DoclogoAndNameWidget()]),
         ),
       ),
     );
